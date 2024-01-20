d73c774715513f9d2d66bb2a8796c725c03df959
AdamArabov <arabovadam@yahoo.com>
2024-01-20 15:07:24 -0500

    Recover Password done

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -13,24 +13,13 @@ import classes from './index.module.scss'
 export default async function RecoverPassword() {
   return (
     <section className={classes.recoverPassword}>
-      <div className={classes.heroImg}>
-        <Link href="/">
-          <Image
-            src="/logo-black.svg"
-            alt="logo"
-            width={250}
-            height={23}
-            className={classes.logo}
-          />
-        </Link>
-      </div>
-
+      <div className={classes.heroImg}></div>
       <div className={classes.formWrapper}>
         <div className={classes.formContainer}>
           <RenderParams className={classes.params} />
 
           <Link href="/login" className={classes.backLink}>
-            <Image src="/assets/icons/arrow-left.svg" alt="left arrow" width={24} height={24} />
+            <Image src="icons/left.svg" alt="left arrow" width={24} height={24} />
             <p>Back</p>
           </Link>
           <div className={classes.formTitle}>
