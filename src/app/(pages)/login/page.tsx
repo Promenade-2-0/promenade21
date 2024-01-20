f72bb5f4b08030c9f83f6090efd05d3b565d5230
AdamArabov <arabovadam@yahoo.com>
2024-01-20 14:05:28 -0500

    changed up the login page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -18,29 +18,14 @@ export default async function Login() {
 
   return (
     <section className={classes.login}>
-      <div className={classes.heroImg}>
-        <Link href="/">
-          <Image
-            src="/logo-black.svg"
-            alt="logo"
-            width={250}
-            height={23}
-            className={classes.logo}
-          />
-        </Link>
-      </div>
-
+      <div className={classes.heroImg}></div>
       <div className={classes.formWrapper}>
         <div className={classes.formContainer}>
           <RenderParams className={classes.params} />
 
           <div className={classes.formTitle}>
-            <h3>Welcome</h3>
-            <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30} />
+            <Image src="/logo.png" alt="hand" width={200} height={30} />
           </div>
-
-          <p>Please login here</p>
-
           <LoginForm />
         </div>
       </div>
